microscopium/microscopium

commit c210b0cecf2309e896c3accc10df9b7b465fffd9

Illumination field is average, not sum

husc/preprocess.py
@@ -160,7 +160,12 @@ def find_background_illumination(im_iter, radius=51, quantile=0.05):
     bg_iter = it.imap(qfilter, im_iter)
     im0 = bg_iter.next()
     accumulator = np.zeros(im0.shape, float)
-    illum = reduce(lambda x, y: x + y, it.chain([im0], bg_iter), accumulator)
+    bg_iter = it.chain([im0], bg_iter)
+    counter = it.count()
+    illum = reduce(lambda x, y: x[0] + y[0],
+                   it.izip(bg_iter, counter), accumulator)
+    n_images = counter.next()
+    illum /= n_images
     return illum
 
 

commit a851504c8635531ddad3ca6845e41eeed575b3bf

Bloody bug fix. Thank you git bisect. illum works
Illumination estimation was failing miserably for some reason. Turns
out that adding a counter broke the `reduce` operation, because the
operand was an array while the right one was a tuple, instead of both
being tuples. For future bug historians, the bug was introduced in
c210b0cecf2309e896c3accc10df9b7b465fffd9, and fixed here.

husc/preprocess.py
@@ -243,7 +243,7 @@ def find_background_illumination(im_iter, radius=51, quantile=0.05):
     accumulator = np.zeros(im0.shape, float)
     bg_iter = it.chain([im0], bg_iter)
     counter = it.count()
-    illum = reduce(lambda x, y: x[0] + y[0],
+    illum = reduce(lambda x, y: x + y[0],
                    it.izip(bg_iter, counter), accumulator)
     n_images = counter.next()
     illum /= n_images