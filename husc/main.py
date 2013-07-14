62ddeac0384c80d9b35aaaa082127a9c2e98af22
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2013-07-14 18:43:41 +1000

    Bug fix: allow saving of illumination field

--- husc/main.py
+++ husc/main.py
@@ -78,7 +78,7 @@ def run_illum(args):
     ims = (mh.imread(fn) for fn in args.images)
     il = pre.find_background_illumination(ims, args.radius, args.quantile)
     if args.save_illumination is not None:
-        io.imsave(il)
+        io.imsave(il, args.save_illumination)
     base_fns = (os.path.splitext(fn)[0] for fn in args.images)
     ims_out = (fn + args.output_suffix for fn in base_fns)
     ims = (mh.imread(fn) for fn in args.images)
