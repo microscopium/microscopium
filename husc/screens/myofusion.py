f9562f9516ddacaca69ba7716cc21cae88bd3a92
starcalibre <starcalibre@gmail.com>
2014-10-07 15:38:07 +1100

    Fixed all doctests

--- husc/screens/myofusion.py
+++ husc/screens/myofusion.py
@@ -86,10 +86,10 @@ def myores_semantic_filename(fn):
     Examples
     --------
     >>> fn = ('MYORES-p1-j01-110210_02490688_53caa10e-ac15-4166-9b9d-'
-              '4b1167f3b9c6_C04_s1_w1.TIF')
+    ...       '4b1167f3b9c6_C04_s1_w1.TIF')
     >>> d = myores_semantic_filename(fn)
     >>> d
-    OrderedDict([('directory', ''), ('prefix', 'MYORES'), ('pass', 'p1'), ('job', 'j01'), ('date', '110210'), ('plate', 2490688), ('barcode', '53caa10e-ac15-4166-9b9d-4b1167f3b9c6'), ('well', 'C04'), ('quadrant', 's1'), ('channel', 'w1'), ('suffix', '')])
+    OrderedDict([('directory', ''), ('prefix', 'MYORES'), ('pass', 'p1'), ('job', 'j01'), ('date', '110210'), ('plate', 2490688), ('barcode', '53caa10e-ac15-4166-9b9d-4b1167f3b9c6'), ('well', 'C04'), ('quadrant', 's1'), ('channel', 'w1'), ('suffix', 'TIF')])
     """
     keys = ['directory', 'prefix', 'pass', 'job', 'date', 'plate',
             'barcode', 'well', 'quadrant', 'channel', 'suffix']
@@ -125,7 +125,7 @@ def filename2coord(fn):
     Examples
     --------
     >>> fn = ('MYORES-p1-j01-110210_02490688_53caa10e-ac15-4166-9b9d-'
-              '4b1167f3b9c6_C04_s1_w1.TIF')
+    ...       '4b1167f3b9c6_C04_s1_w1.TIF')
     >>> filename2coord(fn)
     (2490688, 'C04')
     """
