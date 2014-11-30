0415d3afeb8ace2c8cf8e3a9df8a602f1a7db905
Adrian <adrian@adrian-G30AB.(none)>
2014-12-01 10:12:04 +1100

    Added mongoDB query to distance function

--- husc/metrics.py
+++ husc/metrics.py
@@ -1,7 +1,6 @@
-from collections import defaultdict
 from itertools import combinations
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
+from scipy.spatial.distance import pdist
 
 def sq_to_dist(i, j, n):
     """Convert coordinate of square distance matrix to condensed matrix index.
@@ -46,16 +45,15 @@ def sq_to_dist(i, j, n):
     return index
 
 
-def gene_distance_score(X, gene_list, metric='euclidean'):
+def gene_distance_score(X, collection, metric='euclidean'):
     """Find intra/extra gene distance scores between samples.
 
     Parameters
     ----------
-    X : array, shape (n_samples, n_features)
-        The data matrix.
-    gene_list : array, shape (n_samples, )
-        Array indicating which gene was knocked-down
-        corresponding to each sample.
+    X : Data frame, shape (n_samples, n_features)
+        Feature data frame.
+    collection : pymongo collection
+        Pymongo object directing to collection.
     metric : string, optional
         Which distance measure to use when calculating distances.
         Must be one of the options allowable in
@@ -63,44 +61,54 @@ def gene_distance_score(X, gene_list, metric='euclidean'):
 
     Returns
     -------
-    all_intragene_data : array
+    all_intergene_data : array
         An 1D array with intra-gene distances (i.e. distances
         between samples with the same gene knocked down).
     all_extragene_data : array
         An 1D array with extra-gene distances (i.e. distances
         between samples with the same gene knocked down).
 
-    Examples
-    --------
-    ## TODO make this doctest much prettier or make it a full test
-    >>> data = np.zeros((6, 3))
-    >>> data[0:2, :] = 1
-    >>> data[2:4, :] = 4
-    >>> data[4:6, :] = 7
-    >>> genes = ['A', 'A', 'B', 'B', 'C', 'C']
-    >>> intra, extra = gene_distance_score(data, genes, 'euclidean')
-    >>> intra
-    array([ 0.,  0.,  0.])
-    >>> extra
-    array([  5.19615242,   5.19615242,  10.39230485,  10.39230485,
-             5.19615242,   5.19615242,  10.39230485,  10.39230485,
-             5.19615242,   5.19615242,   5.19615242,   5.19615242])
     """
-    all_intragene_index = []
-    gene_index = defaultdict(list)
 
-    for key, value in zip(gene_list, range(len(gene_list))):
-        gene_index[key].append(value)
+    mongo_query = collection.aggregate([
+        {
+            "$group" : {
+                "_id": "$gene_name",
+                "coords": {
+                    "$addToSet": {
+                        "cell_plate_barcode": "$cell_plate_barcode",
+                        "well": "$well"
+                        }
+                    }
+            }
+        }
+    ])['result']
 
-    for key in gene_index:
-        for i, j in combinations(gene_index[key], 2):
-            all_intragene_index.append(sq_to_dist(i, j, X.shape[0]))
+    # parse mongo query and create (gene) -> (coord) dictionary
+    gene_dict = {}
+    for doc in mongo_query:
+        gene_dict[doc['_id']] = list()
+        for coord in doc['coords']:
+            try:
+                new_coord = (coord['cell_plate_barcode'], str(coord['well']))
+                gene_dict[doc['_id']].append(new_coord)
+            except KeyError:
+                pass
 
+    # get inner indices
+    all_intergene_index = []
+    for key in gene_dict:
+        if len(gene_dict[key]) > 1:
+            indices = map(X.index.get_loc, gene_dict[key])
+            for i, j in combinations(indices, 2):
+                all_intergene_index.append(sq_to_dist(i, j, X.shape[0]))
+
+    all_intergene_index.sort()
     n = sq_to_dist(X.shape[0], X.shape[0], X.shape[0])
-    all_extragene_index = np.setdiff1d(np.arange(n), all_intragene_index)
+    all_extragene_index = np.setdiff1d(np.arange(n), all_intergene_index,
+                                       assume_unique=True)
+    distance = pdist(X, metric)
+    all_intergene_data = distance[all_intergene_index]
+    all_extragene_data = distance[all_extragene_index]
 
-    all_intragene_data = pdist(X, 'euclidean')[all_intragene_index]
-    all_extragene_data = pdist(X, 'euclidean')[all_extragene_index]
-
-    return all_intragene_data, all_extragene_data
-
+    return all_intergene_data, all_extragene_data
