cc31e94370c1a8939ca18042a5cb5a35fcc964f0
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2018-10-12 00:31:55 +1100

    Enhancements to download_data.js
    - Use tabs instead of commas as separators, as it is more robust
    - Use a function to convert a table to tsv text
    - Minor other cleanups

--- microscopium/app/download_data.js
+++ microscopium/app/download_data.js
@@ -1,27 +1,27 @@
-var data = source.data;
-var filetext = 'info,url,x,y\n';
-for (var i = 0; i < data['info'].length; i++) {
-    var currRow = [data['info'][i].toString(),
-                   data['url'][i].toString(),
-                   data['x'][i].toString(),
-                   data['y'][i].toString().concat('\n')];
+function to_tsv(data_table) {
+    var columns = Object.keys(data_table)
+    var nrows = data_table[columns[0]].length
+    var lines = [columns.join('\t')];
 
-    var joined = currRow.join();
-    filetext = filetext.concat(joined);
+    for (var i = 0; i < nrows; i++) {
+        var row = [];
+        for (var j = 0; j < columns.length; j++) {
+            var column = columns[j];
+            row.push(data_table[column][i].toString());
+        }
+        lines.push(row.join('\t'));
+    }
+    filetext = lines.join('\n').concat('\n');
+    return filetext
 }
 
-var filename = 'data_result.csv';
-var blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' });
 
-//addresses IE
-if (navigator.msSaveBlob) {
-    navigator.msSaveBlob(blob, filename);
-} else {
-    var link = document.createElement("a");
-    link = document.createElement('a')
-    link.href = URL.createObjectURL(blob);
-    link.download = filename
-    link.target = "_blank";
-    link.style.visibility = 'hidden';
-    link.dispatchEvent(new MouseEvent('click'))
-}
+var filename = 'data_result.tsv';
+var filetext = to_tsv(source.data)
+var blob = new Blob([filetext], { type: 'text/tsv;charset=utf-8;' });
+var link = document.createElement("a");
+link.href = URL.createObjectURL(blob);
+link.download = filename
+link.target = "_blank";
+link.style.visibility = 'hidden';
+link.dispatchEvent(new MouseEvent('click'))
